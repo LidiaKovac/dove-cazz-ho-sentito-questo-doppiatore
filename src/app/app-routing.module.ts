257707014e336efc19ea5830715de98d3aebcf77
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-07-19 11:13:24 +0200

    bugfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingGuard } from './landing/landing.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then((c) => c.AuthModule),
+    loadChildren: () => import('./features/auth/auth.module').then((c) => c.AuthModule),
   },
   {
     path: 'landing',
-    loadChildren: () =>
-      import('./landing/landing.module').then((c) => c.LandingModule),
+    loadChildren: () => import('./features/landing/landing.module').then((c) => c.LandingModule),
   },
   {
     path: '',
@@ -20,7 +18,7 @@ const routes: Routes = [
   {
     path: 'compare',
     loadChildren: () =>
-      import('./doppiatori/doppiatori.module').then((c) => c.DoppiatoriModule),
+      import('./features/doppiatori/doppiatori.module').then((c) => c.DoppiatoriModule),
   },
 ];
 
