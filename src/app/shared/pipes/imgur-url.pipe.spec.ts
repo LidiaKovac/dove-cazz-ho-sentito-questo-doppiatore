3a072548c87f70e8c3e0192b85448198a8f86b84
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-07-22 17:48:35 +0200

    delete .spec

--- src/app/shared/pipes/imgur-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImgurUrlPipe } from './imgur-url.pipe';
-
-describe('ImgurUrlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ImgurUrlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
